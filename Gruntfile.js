20ab05d80620de648376e2a054e30c7d051d77c2
Brian Woodward <brian.woodward@gmail.com>
2013-11-17 22:03:46 -0500

    feature (plugin) new example site for assemble-contrib-lunr
    Setting up the initial example repo for assemble-contrib-lunr.

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+/*
+ * example-assemble-toc
+ * https://github.com/assemble/example-assemble-toc
+ * Copyright (c) 2013
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    vendor: grunt.file.readJSON('.bowerrc').directory,
+    site: grunt.file.readYAML('_config.yml'),
+
+    // Build HTML from templates and data
+    assemble: {
+      options: {
+        flatten: true,
+        site: '<%= site %>',
+        assets: '_gh_pages/assets',
+        partials: ['templates/includes/*.hbs'],
+        plugins: ['assemble-contrib-lunr'],
+        layouts: 'templates/layouts',
+      },
+      example: {
+        files: {'_gh_pages/': ['templates/*.hbs']}
+      }
+    },
+
+    // Before generating any new files,
+    // remove any previously-created files.
+    clean: {
+      example: ['_gh_pages/*.html']
+    }
+  });
+
+  // Load npm plugins to provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('assemble');
+
+  // Default tasks to be run.
+  grunt.registerTask('default', ['clean', 'assemble']);
+};
